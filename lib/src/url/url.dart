a360bc038254939bac779961f3184c5ff69d48a6
Seven Du <whine.whitish.0u@icloud.com>
2023-09-27 18:48:59 +0800

    chore: WIP

--- lib/src/url/url.dart
+++ lib/src/url/url.dart
@@ -1,16 +1,45 @@
 import '../js_compatibility/json.dart';
 import 'url_search_params.dart';
 
+part '_internal/url.dart';
+
 /// Uniform Resource Locator (URL) is a text string that specifies where a
 /// resource (such as a web page, image, or video) can be found on the Internet.
+///
+/// [MDN reference](https://developer.mozilla.org/en-US/docs/Web/API/URL)
 abstract interface class URL {
+  /// [MDN reference](https://developer.mozilla.org/en-US/docs/Web/API/URL/canParse_static)
+  ///
+  /// Returns a Boolean indicating if the given string is a well-formed URL.
+  static bool canParse(Object url, [Object? base]) {
+    try {
+      URL(url, base);
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
+
+  /// https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL_static
+  ///
+  /// Creates a DOMString containing a URL representing the object given in the
+  /// parameter. The URL lifetime is tied to the document in the window on which
+  /// it was created. The new object URL represents the specified File object or
+  /// Blob object.
+  @Deprecated('Unimplemented') // TODO: URL.createObjectURL()
+  static String createObjectURL(Object object) => throw UnimplementedError();
+
+  /// https://developer.mozilla.org/en-US/docs/Web/API/URL/revokeObjectURL_static
+  ///
+  /// Revokes an object URL previously created using URL.createObjectURL().
+  @Deprecated('Unimplemented') // TODO: URL.revokeObjectURL()
+  static void revokeObjectURL(String url) => throw UnimplementedError();
+
   /// Creates and returns a URL object referencing the URL specified using an
   /// absolute URL string, or a relative URL string and a base URL string.
   ///
   /// [MDN reference](https://developer.mozilla.org/en-US/docs/Web/API/URL/URL)
   factory URL(Object url, [Object? base]) = _URL;
-
-  // TODO: canParse()/createObjectURL()/revokeObjectURL()
 
   /// A string containing a '#' followed by the fragment identifier of the URL.
   ///
@@ -82,175 +111,4 @@ abstract interface class URL {
   ///
   /// [MDN reference](https://developer.mozilla.org/en-US/docs/Web/API/URL/toJSON)
   String toJSON();
-}
-
-final class _URL implements URL {
-  /// Internal, Parses a URL string and returns a `Uri` object.
-  static Uri _parse(Object url) => switch (url) {
-        _URL(uri: final uri) => uri,
-        final String url => Uri.parse(url),
-        final URL url => Uri.parse(url.toString()),
-        final Uri uri => uri,
-        _ => throw ArgumentError.value(url, 'url', 'Invalid URL'),
-      };
-
-  factory _URL(Object url, [Object? base]) {
-    final uri = _parse(url);
-    if (base == null) return _URL._(uri);
-
-    final baseUri = _parse(base);
-    if (baseUri.origin.isEmpty) {
-      throw ArgumentError.value(base, 'base', 'Invalid URL');
-    }
-
-    final value = baseUri.resolveUri(uri);
-    return _URL._(value);
-  }
-
-  _URL._(this.uri) {
-    for (final MapEntry(key: name, value: values)
-        in uri.queryParametersAll.entries) {
-      for (final value in values) {
-        searchParams.append(name, value);
-      }
-    }
-  }
-
-  Uri uri;
-
-  @override
-  String get hash => uri.fragment;
-
-  @override
-  set hash(String value) {
-    uri = uri.replace(fragment: value);
-  }
-
-  @override
-  String get host => uri.hasPort ? '${uri.host}:${uri.port}' : uri.host;
-
-  @override
-  set host(String value) {
-    final pair =
-        value.split(':').where((element) => element.isNotEmpty).toList();
-    if (pair.isEmpty) return;
-    hostname = pair[0];
-
-    if (pair.length == 2) {
-      port = pair[1];
-    }
-  }
-
-  @override
-  String get hostname => uri.host;
-
-  @override
-  set hostname(String value) {
-    uri = uri.replace(host: value);
-  }
-
-  @override
-  String get href => uri.fragment;
-
-  @override
-  set href(String value) {
-    uri = uri.replace(fragment: value);
-  }
-
-  @override
-  String get origin => uri.origin;
-
-  @override
-  String get username => uri.userInfo.split(':').firstOrNull ?? '';
-
-  @override
-  set username(String value) {
-    if (value.isEmpty) return;
-
-    final password = this.password;
-    if (password.isEmpty) {
-      uri = uri.replace(userInfo: value);
-      return;
-    }
-
-    uri = uri.replace(userInfo: '$value:$password');
-  }
-
-  @override
-  String get password => switch (uri.userInfo.split(':')) {
-        Iterable(length: 2, last: final password) => password,
-        _ => '',
-      };
-
-  @override
-  set password(String value) {
-    if (value.isEmpty) return;
-
-    uri = uri.replace(userInfo: '$username:$value');
-  }
-
-  @override
-  String get pathname => uri.path;
-
-  @override
-  set pathname(String value) {
-    uri = uri.replace(path: value);
-  }
-
-  @override
-  String get port => uri.hasPort ? uri.port.toString() : '';
-
-  @override
-  set port(String value) {
-    final port = int.tryParse(value);
-    if (port != null) {
-      uri = uri.replace(port: port);
-    }
-  }
-
-  @override
-  String get protocol => '${uri.scheme}:';
-
-  @override
-  set protocol(String value) {
-    final schema =
-        value.endsWith(':') ? value.substring(0, value.length - 1) : value;
-    if (schema.isEmpty) return;
-
-    uri = uri.replace(scheme: schema);
-  }
-
-  @override
-  String get search {
-    final search = searchParams.toString();
-    return search.isEmpty ? '' : '?$search';
-  }
-
-  @override
-  set search(String value) {
-    uri = uri.replace(query: value);
-    for (final key in searchParams.keys()) {
-      searchParams.delete(key);
-    }
-
-    for (final name in uri.queryParametersAll.keys) {
-      for (final value in uri.queryParametersAll[name] ?? const <String>[]) {
-        searchParams.append(name, value);
-      }
-    }
-  }
-
-  @override
-  URLSearchParams get searchParams => _searchParams;
-  final _searchParams = URLSearchParams();
-
-  @override
-  String toJSON() => JSON.stringify(toString());
-
-  @override
-  String toString() {
-    if (searchParams.size == 0) return uri.toString();
-
-    return uri.replace(query: searchParams.toString()).toString();
-  }
 }
