3b611586ec4bcf5ffa1b51355b26aec317b9eabc
Seven Du <whine.whitish.0u@icloud.com>
2023-12-15 23:26:04 +0800

    chore: WIP

--- 0.1/src/typed_data/array_buffer.dart
+++ 0.1/src/typed_data/array_buffer.dart
@@ -1,7 +1,16 @@
-import 'dart:typed_data';
-
 // typedef ArrayBuffer = ByteBuffer;
 
 // extension ArrayBufferExtension on ArrayBuffer {}
 
-extension type ArrayBuffer {}
+extension type const E(String message) {
+  factory E.two(String message) => message as E;
+
+  void echo() => print(message);
+}
+
+echo(E e) => e.echo();
+
+void main(List<String> args) {
+  const demo = E('1');
+  echo('hello' as E);
+}
