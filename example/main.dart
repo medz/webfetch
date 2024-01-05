084482217a8228c46e4523dbace251f62f1496fc
Seven Du <whine.whitish.0u@icloud.com>
2024-01-05 15:38:32 +0800

    chore: update example

--- example/main.dart
+++ example/main.dart
@@ -8,11 +8,16 @@ void main() async {
   final blob = Blob(['Hello, World!'], type: 'text/plain');
   formData.append('file', blob, 'hello.txt');
 
+  final headers = Headers();
+  headers.append('x-multiple', 'Value 1');
+  headers.append('x-multiple', 'Value 2');
+
   final response = await fetch(
     'https://webhook.site/9e179866-34ba-487d-8538-7c69ca963a01',
     method: "POST",
     body: formData,
     keepalive: true,
+    headers: headers,
   );
 
   print(await response.text());
